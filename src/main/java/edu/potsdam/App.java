e821d06e2f0d33e0e38f4a260dec61f6249577d7
Norman Rosner <normanrosner@googlemail.com>
2011-01-29 14:46:30 +0100

    Initial maven project setup. Already added some ignore patterns for git.

---
+++ src/main/java/edu/potsdam/App.java
@@ -0,0 +1,13 @@
+package edu.potsdam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
